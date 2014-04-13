9da46b83e9e183e6dc2591d04eab43c542c42174
Daniel <potrayarrick@gmail.com>
2014-04-14 00:19:20 +0200

    Test final

--- src/helloworld.py
+++ src/helloworld.py
@@ -1,5 +1,2 @@
 print 'Content-Type: text/plain'
-print ''
-print 'Hello, Google App Engine! -- from CloudBees'
-print 'Si esto se ve es que se ha hecho bien la cosa'
-print 'Si se ve esto todo esta muy bien configurado....'
+print 'Bienvenidos a Looking for Birra'
