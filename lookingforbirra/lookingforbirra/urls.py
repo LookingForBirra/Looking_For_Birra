b8681eeb94f2f15e85decac39507eb9b227ab237
Daniel <potray@users.noreply.github.com>
2014-04-19 12:30:49 +0200

    Update urls.py

--- lookingforbirra/lookingforbirra/urls.py
+++ lookingforbirra/lookingforbirra/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
+    # Examples: 
     # url(r'^$', 'lookingforbirra.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
